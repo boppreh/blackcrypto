e5e97c97a823d62d074c427d9725d157af818834
BoppreH <boppreh@gmail.com>
2014-06-12 20:22:55 -0300

    Add SHA1 extension attack

--- blackcrypto.py
+++ blackcrypto.py
@@ -1,12 +1,12 @@
-from rawhashes import md5, md5pad
+from rawhashes import md5, sha1, pad
 from simplecrypto.hashes import md5 as simplemd5
 from simplecrypto.formats import hex, from_hex, to_bytes
 
-def extend_md5(data, extension, secret_length, secretdata_hash):
+def extend(algo, data, extension, secret_length, secretdata_hash):
 	"""
 	Extends a hashed message without knowing a part of it.
 	Computes (extended_data, appended_hash) such that
-	md5(secret || extended_data) == appended_hash, where `extended_data`
+	hash(secret || extended_data) == appended_hash, where `extended_data`
 	starts with `data` and ends with `extension`.
 	"""
 	data = to_bytes(data)
@@ -14,10 +14,16 @@ def extend_md5(data, extension, secret_length, secretdata_hash):
 	secretdata_hash = to_bytes(secretdata_hash)
 
 	secretdata_length = len(data) + secret_length
-	padded_data = md5pad(data, secretdata_length)
+	padded_data = pad(data, secretdata_length, 'little' if algo == md5 else 'big')
 
 	total_length = secret_length + len(padded_data) + len(extension)
-	return padded_data + extension, md5(extension, total_length, secretdata_hash)
+	return padded_data + extension, algo(extension, total_length, secretdata_hash)
+
+def extend_md5(data, extension, secret_length, secretdata_hash):
+	return extend(md5, data, extension, secret_length, secretdata_hash)
+
+def extend_sha1(data, extension, secret_length, secretdata_hash):
+	return extend(sha1, data, extension, secret_length, secretdata_hash)
 
 if __name__ == '__main__':
 	secret = b'secret'
@@ -25,4 +31,12 @@ if __name__ == '__main__':
 	secretdata_hash = md5(secret + data)
 	appended_data, appended_hash = extend_md5(data, b'append', len(secret), secretdata_hash)
 	print(appended_data)
-	print(hex(appended_hash) == hex(md5(secret + appended_data)))
+	print(hex(appended_hash), '==', hex(md5(secret + appended_data)))
+
+	secret = b'secret'
+	data = b'data'
+	secretdata_hash = sha1(secret + data)
+	
+	appended_data, appended_hash = extend_sha1(data, b'append', len(secret), secretdata_hash)
+	print(appended_data)
+	print(hex(appended_hash), '==', hex(sha1(secret + appended_data)))
--- rawhashes.py
+++ rawhashes.py
@@ -1,14 +1,14 @@
 import math
 import struct
 
-def md5pad(message, length):
-    message = bytearray(message)
-    message.append(0x80)
-    if length != 56:
-        message.extend([0] * (56 - (length + 1) % 64))
-    message += (length * 8).to_bytes(8, byteorder='little')
-    return bytes(message)
+def pad(message, length, byteorder='little'):
+    message += b'\x80'
+    message += b'\x00' * ((56 - (length + 1) % 64) % 64)
+    message += (length * 8).to_bytes(8, byteorder=byteorder)
+    return message
 
+def left_rotate(x, amount):
+    return ((x<<amount) | (x>>(32-amount))) & 0xFFFFFFFF
 
 # Adapted from http://rosettacode.org/wiki/MD5/Implementation#Python
 def md5(message, message_length=None, state=b'\x01#Eg\x89\xab\xcd\xef\xfe\xdc\xba\x98vT2\x10'):
@@ -32,14 +32,10 @@ def md5(message, message_length=None, state=b'\x01#Eg\x89\xab\xcd\xef\xfe\xdc\xb
                       16*[lambda i: (3*i + 5)%16] + \
                       16*[lambda i: (7*i)%16]
 
-    def left_rotate(x, amount):
-        x &= 0xFFFFFFFF
-        return ((x<<amount) | (x>>(32-amount))) & 0xFFFFFFFF
-
     message_length = message_length or len(message)
-    message = md5pad(message, message_length)
+    message = pad(message, message_length, 'little')
  
-    hash_pieces = list(struct.unpack("<LLLL", state))
+    hash_pieces = list(struct.unpack('<LLLL', state))
  
     for chunk_ofst in range(0, len(message), 64):
         a, b, c, d = hash_pieces
@@ -48,10 +44,50 @@ def md5(message, message_length=None, state=b'\x01#Eg\x89\xab\xcd\xef\xfe\xdc\xb
             f = functions[i](b, c, d)
             g = index_functions[i](i)
             to_rotate = a + f + constants[i] + int.from_bytes(chunk[4*g:4*g+4], byteorder='little')
+            to_rotate &= 0xFFFFFFFF
             new_b = (b + left_rotate(to_rotate, rotate_amounts[i])) & 0xFFFFFFFF
             a, b, c, d = d, new_b, b, c
         for i, val in enumerate([a, b, c, d]):
             hash_pieces[i] += val
             hash_pieces[i] &= 0xFFFFFFFF
 
-    return struct.pack("<LLLL", *hash_pieces)
+    return struct.pack('<LLLL', *hash_pieces)
+
+# Adapted from https://github.com/ajalt/python-sha1
+def sha1(message, message_length=None, state=b'gE#\x01\xef\xcd\xab\x89\x98\xba\xdc\xfe\x102Tv\xc3\xd2\xe1\xf0'):
+    hash_pieces = list(struct.unpack('>LLLLL', state))
+    
+    message_length = message_length or len(message)
+    message = pad(message, message_length, 'big')
+
+    for i in range(0, len(message), 64):
+        w = [0] * 80
+        for j in range(16):
+            w[j] = struct.unpack(b'>I', message[i + j*4:i + j*4 + 4])[0]
+        for j in range(16, 80):
+            w[j] = left_rotate(w[j-3] ^ w[j-8] ^ w[j-14] ^ w[j-16], 1)
+    
+        a, b, c, d, e = hash_pieces
+    
+        for i in range(80):
+            if 0 <= i <= 19:
+                f = d ^ (b & (c ^ d))
+                k = 0x5A827999
+            elif 20 <= i <= 39:
+                f = b ^ c ^ d
+                k = 0x6ED9EBA1
+            elif 40 <= i <= 59:
+                f = (b & c) | (b & d) | (c & d) 
+                k = 0x8F1BBCDC
+            elif 60 <= i <= 79:
+                f = b ^ c ^ d
+                k = 0xCA62C1D6
+    
+            a, b, c, d, e = ((left_rotate(a, 5) + f + e + k + w[i]) & 0xffffffff, 
+                            a, left_rotate(b, 30), c, d)
+    
+        for i, val in enumerate([a, b, c, d, e]):
+            hash_pieces[i] += val
+            hash_pieces[i] &= 0xFFFFFFFF
+    
+    return struct.pack('>LLLLL', *hash_pieces)
